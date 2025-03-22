dd8e013927c8ad3c434591264223f53da46d2e84
Andrew <andrew@eisenhawer.ca>
2025-03-21 23:33:44 -0600

    in accordion as well

--- src/lib/pages/Landing.svelte
+++ src/lib/pages/Landing.svelte
@@ -1049,8 +1049,8 @@ async function handleTextSearch(searchText) {  // only for search from search ba
                               {#each result.resources as resource}
                                 <li>
                                   <a href={resource.url} target="_blank" rel="noopener noreferrer">
-                                   {resource.format} {resource.name}
-                                    <svg class="external-link-icon" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                    {resource.name} : {resource.format}
+                                    <svg class="external-link-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                       <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6m4-3h6v6m-11 1L21 3"/></svg>
                         </a>
                       </li>
