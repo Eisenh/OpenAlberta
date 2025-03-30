8703da82bad06c6c860d4ddab699d6c6d1593607
Andrew <andrew@eisenhawer.ca>
2025-03-29 22:02:30 -0600

    updateUser({ password: password });

--- src/lib/pages/ResetPassword.svelte
+++ src/lib/pages/ResetPassword.svelte
@@ -168,7 +168,7 @@
       console.log("Resetting with: ",password," pwd ",supabase.auth)
       const { error: resetError } = await supabase.auth.updateUser({ password: password });
       if (resetError) {
-        console.log("Reset - Error with update: ", error);
+        console.log("Reset - Error with update: ", resetError.message);
         errorMessage = resetError.message;
       } 
       else {  
